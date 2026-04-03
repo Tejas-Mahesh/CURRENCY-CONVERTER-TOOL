e8f0dfdf02218c5ae9afa377d6945cbf6a73b49e
TEJAS M <tejasmanjulakg@gmail.com>
2026-04-03 19:22:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
         USD: 1,      INR: 93.79,   EUR: 0.86,   GBP: 0.79,   JPY: 158.29,
         AUD: 1.51,   CAD: 1.37,   SGD: 1.34,   CHF: 0.79,   CNY: 7.23,
         HKD: 7.82,   NZD: 1.66,   BRL: 5.06,   RUB: 92.4,   ZAR: 18.9,
-        MXN: 16.6,   PHP: 56.1,   THB: 36.4,   SAR: 3.75,   AED: 3.67
+        MXN: 16.6,   PHP: 56.1,   THB: 36.4,   SAR: 3.75,   AED: 3.68
     };
     console.log(rate);
     let result=(amount/rate[from])*rate[to];
